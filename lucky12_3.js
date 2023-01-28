f9a823f1593410c5d8415f3a8fdaa0241560f9b9
hint51 <86897729+hint51@users.noreply.github.com>
2023-01-28 09:27:15 +0900

    Add files via upload

---
+++ lucky12_3.js
@@ -0,0 +1,8 @@
+function know(){
+    var kon = document.getElementById("tip");
+    if(kon.style.display=='none'){
+       kon.style.display = 'block';
+    }else{
+       kon.style.display = 'none';
+    }
+ }
