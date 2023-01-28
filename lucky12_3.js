6ba14f8dab3b115232e2930cb949b475e6bea450
hint51 <86897729+hint51@users.noreply.github.com>
2023-01-28 09:26:11 +0900

    Delete lucky12_3.js

--- lucky12_3.js
+++
@@ -1,8 +0,0 @@
-function know(){
-    var kon = document.getElementById("tip");
-    if(kon.style.display=='none'){
-       kon.style.display = 'block';
-    }else{
-       kon.style.display = 'none';
-    }
- }
