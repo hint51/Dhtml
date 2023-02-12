fc49e8a5b80349acbb58a6725700f815493b22d3
hint51 <86897729+hint51@users.noreply.github.com>
2023-02-12 23:46:08 +0900

    Add files via upload

---
+++ sal4.js
@@ -0,0 +1,79 @@
+function result1() { 
+   document.getElementById("btn1").style.display="none"
+}
+function result2() { 
+   document.getElementById("btn2").style.display="none"
+}
+function result3() { 
+   document.getElementById("btn3").style.display="none"
+}
+function result4() { 
+   document.getElementById("btn4").style.display="none"
+}
+function result5() { 
+   document.getElementById("btn5").style.display="none"
+}
+function result6() { 
+   document.getElementById("btn6").style.display="none"
+}
+function result7() { 
+   document.getElementById("btn7").style.display="none"
+}
+function result8() { 
+   document.getElementById("btn8").style.display="none"
+}
+function result9() { 
+   document.getElementById("btn9").style.display="none"
+}
+function result10() { 
+   document.getElementById("btn10").style.display="none"
+}
+function result11() { 
+   document.getElementById("btn11").style.display="none"
+}
+function result12() { 
+   document.getElementById("btn12").style.display="none"
+}
+function result13() { 
+   document.getElementById("btn13").style.display="none"
+}
+
+function btn_A() { 
+      if(document.getElementById("btn_A").innerText=="전체보이기"){
+       document.getElementById("btn_A").innerHTML="전체숨기기"
+       document.getElementById("btn_A").style.background="right yellow"
+       document.getElementById("btn1").style.display="none"
+       document.getElementById("btn2").style.display="none"
+       document.getElementById("btn3").style.display="none"
+       document.getElementById("btn4").style.display="none"
+       document.getElementById("btn5").style.display="none"
+       document.getElementById("btn6").style.display="none"
+       document.getElementById("btn7").style.display="none"
+       document.getElementById("btn8").style.display="none"
+       document.getElementById("btn9").style.display="none"
+       document.getElementById("btn10").style.display="none"
+       document.getElementById("btn11").style.display="none"
+       document.getElementById("btn12").style.display="none"
+       document.getElementById("btn13").style.display="none"
+       
+       
+
+    } else {
+       document.getElementById("btn_A").innerHTML="전체보이기"
+       document.getElementById("btn_A").style.background="antiquewhite"
+       document.getElementById("btn1").style.display="inline"
+       document.getElementById("btn2").style.display="inline"
+       document.getElementById("btn3").style.display="inline"
+       document.getElementById("btn4").style.display="inline"
+       document.getElementById("btn5").style.display="inline"
+       document.getElementById("btn6").style.display="inline"
+       document.getElementById("btn7").style.display="inline"
+       document.getElementById("btn8").style.display="inline"
+       document.getElementById("btn9").style.display="inline"
+       document.getElementById("btn10").style.display="inline"
+       document.getElementById("btn11").style.display="inline"
+       document.getElementById("btn12").style.display="inline"
+       document.getElementById("btn13").style.display="inline"
+       
+    }
+}
